6f02ab1bf0f21f0fe02eab4e449ade76aabfc1da
MKB713 <bakhadija742@gmail.com>
2025-11-03 19:48:41 +0000

    first commit

---
+++ src/main/java/com/uasz/Atelier1_Gestion_Enseignements_UASZ/Atelier1GestionEnseignementsUaszApplication.java
@@ -0,0 +1,13 @@
+package com.uasz.Atelier1_Gestion_Enseignements_UASZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atelier1GestionEnseignementsUaszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atelier1GestionEnseignementsUaszApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uasz/Atelier1_Gestion_Enseignements_UASZ/Atelier1GestionEnseignementsUaszApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uasz.Atelier1_Gestion_Enseignements_UASZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atelier1GestionEnseignementsUaszApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
