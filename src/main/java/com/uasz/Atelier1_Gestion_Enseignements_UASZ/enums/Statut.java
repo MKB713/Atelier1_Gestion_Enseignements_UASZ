5cf38b8ff4683657bf05d7fc97a0f8826280d314
MKB713 <bakhadija742@gmail.com>
2025-11-08 02:29:34 +0000

    problème de tables enseignants réglé

--- src/main/java/com/uasz/Atelier1_Gestion_Enseignements_UASZ/enums/Statut.java
+++ src/main/java/com/uasz/Atelier1_Gestion_Enseignements_UASZ/enums/Statut.java
@@ -1,22 +1,5 @@
 package com.uasz.Atelier1_Gestion_Enseignements_UASZ.enums;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "Enseignant")
 public enum Statut {
     PERMANENT, VACATAIRE;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Id
-    private Long id;
 }
