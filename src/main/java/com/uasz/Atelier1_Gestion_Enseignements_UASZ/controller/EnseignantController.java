8c4cdc94943565ac177d6e79383fb0637f628d42
MKB713 <bakhadija742@gmail.com>
2025-11-12 14:29:25 +0000

    ajout du formulaire de liste enseignants archivés

--- src/main/java/com/uasz/Atelier1_Gestion_Enseignements_UASZ/controller/EnseignantController.java
+++ src/main/java/com/uasz/Atelier1_Gestion_Enseignements_UASZ/controller/EnseignantController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/enseignants")
+
 public class EnseignantController {
 
     @Autowired
