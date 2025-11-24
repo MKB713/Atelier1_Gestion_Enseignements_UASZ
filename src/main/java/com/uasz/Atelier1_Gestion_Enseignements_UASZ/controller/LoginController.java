21134ec2e131d687a0e2245f3c7cbc42c76e2ac0
Makhou <diagnemakhar2004@gmail.com>
2025-11-24 05:49:32 +0000

    Person sidebar2 et redirection

--- src/main/java/com/uasz/Atelier1_Gestion_Enseignements_UASZ/controller/LoginController.java
+++ src/main/java/com/uasz/Atelier1_Gestion_Enseignements_UASZ/controller/LoginController.java
@@ -2,7 +2,6 @@ package com.uasz.Atelier1_Gestion_Enseignements_UASZ.controller;
 
 import com.uasz.Atelier1_Gestion_Enseignements_UASZ.dto.UserDTO;
 import com.uasz.Atelier1_Gestion_Enseignements_UASZ.services.CustomUserDetails;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class LoginController {
 
     private final AuthenticationManager authenticationManager;
+    private final GlobalController globalController;
 
     @GetMapping("/login")
     public String index() {
@@ -28,17 +28,38 @@ public class LoginController {
     @PostMapping("/auth2")
     public String signIn(Model model, UserDTO userDTO, HttpSession session) {
         try {
-
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(userDTO.getEmail(), userDTO.getPassword())
             );
-            System.out.println("Connexion réussie");
             SecurityContextHolder.getContext().setAuthentication(authentication);
             session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
+            CustomUserDetails currentUser = globalController.getCurrentUser();
 
-            return "redirect:/lst-enseignants";
+            if (currentUser != null) {
+                session.setAttribute("currentUser", currentUser);
+                for (var authority : currentUser.getAuthorities()) {
+                    String role = authority.getAuthority();
+                    if ("ROLE_ADMIN".equals(role)) {
+                        return "redirect:/dash-coordinateur";
+                    }
+                    else if ("ROLE_RESPONSABLE_MASTER".equals(role)) {
+                        return "redirect:/dash-enseignant";
+                    } else if ("ROLE_ETUDIANT".equals(role)) {
+                        return "redirect:/dash-etudiant";
+                    } else if ("ROLE_COORDONATEUR_DES_LICENCES".equals(role)) {
+                        return "redirect:/dash-coordinateur";
+                    }
+                    else if ("CHEF_DE_DEPARTEMENT".equals(role)) {
+                        return "redirect:/lst-enseignants";
+                    }
+                }
+
+            }
+            model.addAttribute("errorMessage", "Rôle non reconnu");
+            return "login";
 
         } catch (Exception e) {
+            e.printStackTrace();
             model.addAttribute("errorMessage", "Email ou mot de passe incorrect");
             return "login";
         }
